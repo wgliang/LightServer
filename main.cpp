66fdfa1fb48eabb8be932d9b931fd9153f293da1
wgliaing <liangcszzu@gmail.com>
2015-06-26 16:51:05 +0800

    0.1.3
    完成main函数

--- main.cpp
+++ main.cpp
@@ -3,6 +3,22 @@
 #include <stdlib.h>
 
 int main(){
+	int serverfd = -1;
+	int clientfd = -1;
+	struct sockaddr_in client = NULL;
+	int len_client = sizeof(client);
+	short port;
+	pthread_t thread;
+	//开启服务，初始化server
+	serverfd = start(port);
+	printf("Lightserver is running on port %d\n now!", port);
+	for(;;) {
+		clientfd = accept_request(serverfd, (struct sockaddr *)&client, &len_client);
+		if(clientfd == -1) {
+			error("accept");
+		}
 
+	}
+	close(serverfd);
 	return 0;
 }
---
+++ main.h
@@ -0,0 +1,23 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <strings.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <pthread.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include "status.h"
+#include "helper.h"
+#include "lightserver.h"
+
+#define SERVER_VERSION "Server: Lighthttpd/1.0.0\r\n"
+
+#endif
